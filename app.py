e5d7250f2ee706e49f32afc40b943d8b32a732d6
Taited <taited9160@gmail.com>
2023-07-26 17:15:29 +0800

    huge improvement on the ui

--- app.py
+++ app.py
@@ -1,53 +1,104 @@
-import os
 import os.path as osp
-
+import math
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi import FastAPI, Request, Depends, HTTPException, status, APIRouter, Form
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 from utils import get_file_names, concat_page_data
+from datetime import timedelta
+from api_token import *
 
 
 DIR = '/mnt/tedsun/cafidata/SSDiff_vis'
+TOTAL_NUM = 11000#1700
+PAGE_LIMIT = 20
+TOTAL_PAGE = math.ceil(TOTAL_NUM / PAGE_LIMIT)
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app_protected = APIRouter(dependencies=[Depends(verify_token), Depends(verify_key)])
+app.include_router(app_protected)
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can also specify the exact domain names, e.g., ["https://your-frontend-domain.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/image/static", StaticFiles(directory="static"), name="static")
 
 
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
-async def render_home(request: Request, proj_name: str):
-    base_dir = osp.join(DIR, proj_name)
+def pagination(page: int = 1, limit: int = 10, total: int = TOTAL_NUM):
+    begin_id = (page - 1) * limit + 1
+    if begin_id + limit > total:
+        limit = total - begin_id
+    return begin_id, limit
 
-    project_list = get_file_names(base_dir)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request})
+
+@app.get("/image/", response_class=HTMLResponse)
+async def render_home(request: Request):
+                    #   current_user: User = Depends(get_current_active_user)):
+
+    project_name = get_file_names(DIR)
+    project_list = []
+    for name in project_name:
+        project_list.append(
+            (name, osp.join('/image/show', name, '1'))
+        )
 
     return templates.TemplateResponse(
         "home.html", 
         {"request": request, "project_list": project_list})
 
 
-@app.get("/show/{proj_name}", response_class=HTMLResponse)
-async def render_page(request: Request, proj_name: str):
+@app.get("/image/show/{proj_name}/{page}", response_class=HTMLResponse)
+async def render_page(request: Request, proj_name: str, page: int = 1):
     base_dir = osp.join(DIR, proj_name)
 
     category = get_file_names(base_dir)
+    begin_id, valid_limit = pagination(page, limit=PAGE_LIMIT)
+    page_data = concat_page_data(proj_name, category, begin_id, valid_limit)
     
-    page_data = concat_page_data(proj_name, category, 1, 10)
+    # page navigation
+    begin_page = page - 5 if (page - 5) > 0 else 1
+    end_page = begin_page + 9 if (begin_page + 9) <= TOTAL_PAGE else TOTAL_PAGE
+    page_list = []
+    for page_id in range(begin_page, end_page, 1):
+        page_list.append(
+            (page_id, osp.join('/image/show', proj_name, str(page_id)))
+        )
+    
+    # last and next
+    last_page = page - 1 if (page - 1) > 0 else 1 
+    next_page = page + 1 if (page + 1) <= TOTAL_PAGE else TOTAL_PAGE
+    last_page_url = osp.join('/image/show', proj_name, str(last_page))
+    next_page_url = osp.join('/image/show', proj_name, str(next_page))
 
     return templates.TemplateResponse(
         "page.html", 
         {"request": request, "page_data": page_data,
-         "category": category})
+         "category": category, "page_list": page_list,
+         "last_page": last_page_url, "next_page": next_page_url})
 
 
-@app.get("/image_bed/{project}/{category}/{item_name}")
+@app.get("/image/image_bed/{project}/{category}/{item_name}")
 async def download_files_stream(project: str, category: str, item_name: str):
     file_like = osp.join(DIR, project, category, item_name)
+    if not osp.exists(file_like):
+        item_name = item_name.replace('.png', '')
+        item_name = item_name.zfill(4) + '.png'
+        file_like = osp.join(DIR, project, category, item_name)
     return FileResponse(file_like)
 
 
@@ -55,8 +106,8 @@ if __name__ == '__main__':
     uvicorn.run(
         "app:app",
         reload=True,
-        port=5010,
-        host="localhost",
-        ssl_keyfile="./key.pem",
-        ssl_certfile="./cacert.pem"
+        port=5001,
+        host="0.0.0.0",
+        ssl_keyfile="./select.key",
+        ssl_certfile="./select.crt"
     )
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import os
 import os.path as osp
 
 
+DIR = '/mnt/tedsun/cafidata/SSDiff_vis'
+
 def get_file_names(path: str):
     file_list = []
     for file_name in os.listdir(path):
@@ -13,14 +15,23 @@ def get_file_names(path: str):
 
 def concat_page_data(project_name, category_list: list, 
                      id_begin: int = 1, fetch_num: int = 10,
-                     api_path: str = '/image_bed'):
+                     api_path: str = '/image/image_bed'):
     assert id_begin >= 1, "The begin id could not less than 1 but get: {}".format(id_begin)
     page_data = []
     for id_name in range(id_begin, id_begin + fetch_num):
         row_data = []
         for category_name in category_list:
-            row_data.append(
-                osp.join(api_path, project_name, 
-                         category_name, '{}.png'.format(id_name)))
+            if category_name == 'text_info' or category_name == 'txt':
+                txt_flag = True
+                data_api = osp.join(DIR, project_name, 
+                                    category_name, '{}.txt'.format(id_name))
+                with open(data_api, 'r') as fp:
+                    data_api = fp.read()
+                    fp.close()
+            else:
+                txt_flag = False
+                data_api = osp.join(api_path, project_name, 
+                                    category_name, '{}.png'.format(id_name))
+            row_data.append((txt_flag, data_api))
         page_data.append((id_name, row_data))
     return page_data
