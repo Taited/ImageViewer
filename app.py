f93f738c06f66f7a08c1aa7f6bc6f506113a2c49
Taited <taited9160@gmail.com>
2024-10-10 16:32:23 +0800

    fix bugs in pagination

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import os.path as osp
-import math
-import uvicorn
 import json
+import math
+import os.path as osp
+
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from utils import get_file_names, concat_page_data
 
+from utils import concat_page_data, get_file_names
 
 JSON_DIR = 'jsons'
 IMAGE_DIR = '/data_lg/wentai/codes/vton/experiments/'
-TOTAL_NUM = 11000#1700
+TOTAL_NUM = 11000  # 1700
 PAGE_LIMIT = 20
 TOTAL_PAGE = math.ceil(TOTAL_NUM / PAGE_LIMIT)
 
@@ -20,15 +21,14 @@ app = FastAPI()
 # Set up CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can also specify the exact domain names, e.g., ["https://your-frontend-domain.com"]
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
-app.mount("/image/static", StaticFiles(directory="static"), name="static")
+app.mount('/image/static', StaticFiles(directory='static'), name='static')
 
-
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory='templates')
 
 
 def pagination(page: int = 1, limit: int = 10, total: int = TOTAL_NUM):
@@ -38,61 +38,68 @@ def pagination(page: int = 1, limit: int = 10, total: int = TOTAL_NUM):
     return begin_id, limit
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html", {"request": request})
+    return templates.TemplateResponse('index.html', {'request': request})
 
-@app.get("/image/", response_class=HTMLResponse)
+
+@app.get('/image/', response_class=HTMLResponse)
 async def render_home(request: Request):
-                    #   current_user: User = Depends(get_current_active_user)):
+    #   current_user: User = Depends(get_current_active_user)):
 
     project_name = get_file_names(JSON_DIR)
     project_list = []
     for name in project_name:
-        project_list.append(
-            (name, osp.join('/image/show', name, '1'))
-        )
+        project_list.append((name, osp.join('/image/show', name, '1')))
 
-    return templates.TemplateResponse(
-        "home.html", 
-        {"request": request, "project_list": project_list})
+    return templates.TemplateResponse('home.html', {
+        'request': request,
+        'project_list': project_list
+    })
 
 
-@app.get("/image/show/{proj_name}/{page}", response_class=HTMLResponse)
+@app.get('/image/show/{proj_name}/{page}', response_class=HTMLResponse)
 async def render_page(request: Request, proj_name: str, page: int = 1):
     json_path = osp.join(JSON_DIR, f'{proj_name}.json')
     with open(json_path, 'r') as fp:
         data_dict = json.load(fp)
 
     category = list(data_dict.keys())
-    maxmimum_page = min(TOTAL_PAGE, len(data_dict[category[0]]) // PAGE_LIMIT + 1)
-    begin_id, valid_limit = pagination(page, limit=PAGE_LIMIT, total=maxmimum_page)
+    maximum_item = len(data_dict[category[0]])
+    begin_id, valid_limit = pagination(page,
+                                       limit=PAGE_LIMIT,
+                                       total=maximum_item)
     page_data = concat_page_data(data_dict, category, begin_id, valid_limit)
-    
+
     # page navigation
     begin_page = page - 5 if (page - 5) > 0 else 1
-    end_page = begin_page + 9 if (begin_page + 9) <= maxmimum_page else maxmimum_page
+    maximum_page = min(TOTAL_PAGE,
+                       len(data_dict[category[0]]) // PAGE_LIMIT + 1)
+    end_page = begin_page + 9 if (begin_page +
+                                  9) <= maximum_page else maximum_page
     page_list = []
     for page_id in range(begin_page, end_page, 1):
         page_list.append(
-            (page_id, osp.join('/image/show', proj_name, str(page_id)))
-        )
-    
+            (page_id, osp.join('/image/show', proj_name, str(page_id))))
+
     # last and next
-    last_page = page - 1 if (page - 1) > 0 else 1 
-    next_page = page + 1 if (page + 1) <= maxmimum_page else maxmimum_page
+    last_page = page - 1 if (page - 1) > 0 else 1
+    next_page = page + 1 if (page + 1) <= maximum_page else maximum_page
     last_page_url = osp.join('/image/show', proj_name, str(last_page))
     next_page_url = osp.join('/image/show', proj_name, str(next_page))
 
     return templates.TemplateResponse(
-        "page.html", 
-        {"request": request, "page_data": page_data,
-         "category": category, "page_list": page_list,
-         "last_page": last_page_url, "next_page": next_page_url})
+        'page.html', {
+            'request': request,
+            'page_data': page_data,
+            'category': category,
+            'page_list': page_list,
+            'last_page': last_page_url,
+            'next_page': next_page_url
+        })
 
 
-@app.get("/image/image_bed/{item_name:path}")
+@app.get('/image/image_bed/{item_name:path}')
 async def download_files_stream(item_name: str):
     file_like = osp.join(IMAGE_DIR, item_name)
     return FileResponse(file_like)
@@ -100,10 +107,10 @@ async def download_files_stream(item_name: str):
 
 if __name__ == '__main__':
     uvicorn.run(
-        "app:app",
+        'app:app',
         reload=True,
         port=5001,
-        host="0.0.0.0",
+        host='0.0.0.0',
         # ssl_keyfile="./select.key",
         # ssl_certfile="./select.crt"
     )
--- utils.py
+++ utils.py
@@ -11,23 +11,29 @@ def get_file_names(path: str):
     file_list.sort()
     return file_list
 
-def concat_page_data(data_dict: dict, category_list: list, 
-                     id_begin: int = 1, fetch_num: int = 10,
+
+def concat_page_data(data_dict: dict,
+                     category_list: list,
+                     id_begin: int = 1,
+                     fetch_num: int = 10,
                      api_path: str = '/image/image_bed'):
-    assert id_begin >= 0, "The begin id could not less than 1 but get: {}".format(id_begin)
+    assert id_begin >= 0, 'The begin id could not less than 1 but get'
+    ': {}'.format(id_begin)
     page_data = []
     for id_name in range(id_begin, id_begin + fetch_num + 1):
         row_data = []
         for category_name in category_list:
             if category_name == 'text_info' or category_name == 'txt':
                 txt_flag = True
-                data_api = osp.join(api_path, data_dict[category_name][id_name])
+                data_api = osp.join(api_path,
+                                    data_dict[category_name][id_name])
                 with open(data_api, 'r') as fp:
                     data_api = fp.read()
                     fp.close()
             else:
                 txt_flag = False
-                data_api = osp.join(api_path, data_dict[category_name][id_name])
+                data_api = osp.join(api_path,
+                                    data_dict[category_name][id_name])
             row_data.append((txt_flag, data_api))
         page_data.append((id_name, row_data))
     return page_data
